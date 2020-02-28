79e4aeb2a561f183750678dc27eb5fb1427ba121
EC2 Default User <ec2-user@ip-172-31-44-71.us-east-2.compute.internal>
2020-02-28 01:38:59 +0000

    2.27.1

---
+++ core-python-getting-started/scalar-operators-controlflow/1.py
@@ -0,0 +1,10 @@
+num = -3
+
+if num > 0 and num <= 5:
+    print("num is greater than zero!")
+elif num > 5:
+    print("num is greater than 5!")
+elif num < 0 and num >= -5:
+    print("num is less than zero!")
+else:
+    print("num is less than -5!")
