03dc315b48b0129e0f1f6a84fc5049112a55ac8c
EC2 Default User <ec2-user@ip-172-31-44-71.us-east-2.compute.internal>
2020-02-29 17:36:10 +0000

    revised 1.py

--- core-python-getting-started/scalar-operators-controlflow/1.py
+++ core-python-getting-started/scalar-operators-controlflow/1.py
@@ -1,10 +1,22 @@
-num = -3
+while True:
+    print("Please enter an integer, or type 'stop' to exit.")
+    num = input()
+    if num == "stop":
+        break
+    else:
+        try:
+            num = int(num)
+            if num > 0 and num <= 5:
+                print("num is greater than zero!")
+            elif num > 5:
+                print("num is greater than 5!")
+            elif num < 0 and num >= -5:
+                print("num is less than zero!")
+            elif num < -5:
+                print("num is less than -5!")
+            elif num == 0:
+                print("The number is Zero!")
+        except ValueError:
+            print("You did not enter a valid integer.")
 
-if num > 0 and num <= 5:
-    print("num is greater than zero!")
-elif num > 5:
-    print("num is greater than 5!")
-elif num < 0 and num >= -5:
-    print("num is less than zero!")
-else:
-    print("num is less than -5!")
+    
